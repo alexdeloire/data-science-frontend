d38efe2c263b855420696ece44d8d172d0d79482
alexandre <alexandre.deloire01@etu.umontpellier.fr>
2024-01-14 16:59:29 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import { Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Map from './components/Map';
 import ModelChat from './components/ModelChat';
 import './App.css';
 
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/map">Map</Link>
+        </li>
+        <li>
+          <Link to="/chat">Chat</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
 function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route path="/" element={<Home />} />
-        <Route path="/map" element={<Map />} />
-        <Route path="/chat" element={<ModelChat />} />
-      </Route>
-    </Routes>
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/chat" element={<ModelChat />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
